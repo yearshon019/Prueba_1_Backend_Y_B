52d83a0a24436279fc0872045ab6688629c03a75
Benja <benjamindiazcastillo@gmail.com>
2025-09-11 00:43:00 -0300

    Realizacion de la HU5 a HU8

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "dashboard"
+LOGOUT_REDIRECT_URL = "login"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from devices.views import dashboard, device_list, device_detail, measurement_list
+from devices.views import dashboard, device_list, device_detail, measurement_list, alerts_week, login_view, logout_view, register_view, password_reset_request
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('', dashboard, name='dashboard'),
     path('devices/device_list/', device_list, name='device_list'),
     path('devices/<int:device_id>/', device_detail, name='device_detail'),
     path('devices/measurements/', measurement_list, name='measurement_list'),
+    path('alerts/week/', alerts_week, name="alerts_week"),
+
+    # auth
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+    path('register/', register_view, name="register"),
+    path('password-reset/', password_reset_request, name="password_reset"),
     ]
