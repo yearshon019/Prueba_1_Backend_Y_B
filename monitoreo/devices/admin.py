3415a27afc96c0c9add9974cb3642692bc30d11a
yearshon019 <yearshon.yoain.orrego@gmail.com>
2025-09-11 17:37:55 +0100

    historia de H1 HASTA H4 TERMINADO

--- monitoreo/devices/admin.py
+++ monitoreo/devices/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Zone, Device, Measurement, Alert
+from .models import Category, Zone, Device, Measurement, Alert, Organization
 
 admin.site.register(Category)
 admin.site.register(Zone)
 admin.site.register(Device)
 admin.site.register(Measurement)
 admin.site.register(Alert)
+admin.site.register(Organization)
